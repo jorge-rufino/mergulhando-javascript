8b910507c902179aee1e3bb3c5a5b6e03e1c6d23
jorge-rufino <jorge.rufino1986@gmail.com>
2023-12-11 16:13:26 -0300

    Array de objetos e variacoes For

--- script.js
+++ script.js
@@ -1,9 +1,37 @@
-var person = {
+var person1 = {
 	name: 'Jorge Rufino',
-	age: 35,
+	age: 37,
 	sex: 'male'
 };
 
-person.age = 37;
+var person2 = {
+	name: 'Nickolas Rufino',
+	age: 4,
+	sex: 'male'
+};
 
-console.log(person);
+var person3 = {
+	name: 'Larissa',
+	age: 28,
+	sex: 'female'
+};
+
+var listPerson = [person1, person2, person3];
+
+for (var i =0; i < listPerson.length; i++){
+	console.log(listPerson[i]);
+}
+
+console.log('\n');
+
+for (var person of listPerson){
+	console.log(person);
+}
+
+console.log('\n');
+
+listPerson.forEach(element => {
+	if(element.age < 30){
+		console.log(element);
+	}
+});
