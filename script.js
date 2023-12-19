c167078cb309a95c7c2723825e45f084052898c8
jorge-rufino <jorge.rufino1986@gmail.com>
2023-12-19 08:40:42 -0300

    Clonando objeto com Spread

--- script.js
+++ script.js
@@ -1,35 +1,22 @@
-const parts = ['ombro', 'joelho'];
+const user = {
+	name: 'danielbonifacio',
+	email: 'contato@danielbonifacio.com.br'
+};
 
-//Desse jeito, sempre que fosse adicionado um valor no array acima, teriamos que adicionar no array abaixo
-//const sentence = ['cabeça', parts[0],parts[1], 'pés'];
+//Cria apenas uma referencia ao objeto "user"
+//const newUser = user;
+//newUser.email = 'joao@zinho.com';
+//newUser.name = 'joaozinho123';
+//console.log(user);
+//console.log(newUser);
 
-//Desta forma fica dinâmico
-const sentence = ['cabeça', ...parts, 'pés'];
-console.log(sentence);
+//Clona o objeto
+const newUser = {...user};
 
-/*
-function createUser(name, age, contact1, contact2, contact3) {
-	return {
-		name,
-		age,
-		contacts: [contact1,contact2,contact3]
-	};
-}
-*/
-function createUser(name, age, ...contacts) {
-	return {
-		name,
-		age,
-		contacts
-	};
-}
+console.log(user);
+console.log(newUser);
 
-console.log(createUser('Jorge',37,'email1','email2'));
+newUser.email = 'joao@zinho.com';
+newUser.name = 'joaozinho123';
 
-const immutableArray = ['select', '*', 'from', 'posts'];
-
-//Desta forma estamos apenas criando uma referencia ao objeto "immutableArray", então qualquer alteraçao em "mutableArray" irá para "immutableArray"
-//const mutableArray = immutableArray;
-
-const mutableArray = [...immutableArray];
-mutableArray.push('where id = 1');
+console.log(newUser);
