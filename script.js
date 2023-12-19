757b3ab2f729c8df6a92515565415375d8041495
jorge-rufino <jorge.rufino1986@gmail.com>
2023-12-19 14:03:48 -0300

    Arrays a partir de listas iteráveis com Array.from

--- script.js
+++ script.js
@@ -1,22 +1,12 @@
-const user = {
-	name: 'danielbonifacio',
-	email: 'contato@danielbonifacio.com.br'
-};
+function greet () {
+	//Lembrando, "arguments" é uma LISTA especial (não é um ARRAY), portanto não podemos usar o "foreach", por isso criamos o Array "args" com base em "arguments"
+	const args = Array.from(arguments, function (nome) {
+		return  '!' + nome + '!';
+	});
+	
+	args.forEach(argument => {
+		console.log(`Hello, ${argument}`);
+	});
+}
 
-//Cria apenas uma referencia ao objeto "user"
-//const newUser = user;
-//newUser.email = 'joao@zinho.com';
-//newUser.name = 'joaozinho123';
-//console.log(user);
-//console.log(newUser);
-
-//Clona o objeto
-const newUser = {...user};
-
-console.log(user);
-console.log(newUser);
-
-newUser.email = 'joao@zinho.com';
-newUser.name = 'joaozinho123';
-
-console.log(newUser);
+greet('Jorge','Nick','Lari');
