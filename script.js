d5a283f616bb433c0478c3ded470ed70a7ac8ba1
jorge-rufino <jorge.rufino1986@gmail.com>
2023-12-19 15:48:43 -0300

    Reduzindo uma lista à um único valor com Array.reduce

--- script.js
+++ script.js
@@ -6,9 +6,23 @@ const students = [
 	{ name: 'Marcos',   grade: 5 },
 ];
 
-//Retorna uma array com os elementos baseados na condição do "return"
-const failedStudents = students.filter( student => {
-	return student.grade < 7;
-});
+//Queremos saber os pontos totais da turma, abaixo de 30 pontos precisará de atenção
 
-console.log(failedStudents);
+//Forma antiga de fazer. Gera efeito colateral pois se executarmos o laço varias vezes vai aumentar o "totalGrade"
+//let totalGrade = 0;
+//students.forEach( student => {	totalGrade += student.grade; });
+
+//Ele recebe 2 parametros, o primeiro é uma função e o segundo é o valor inicial que no caso é 0
+//"Prev" está recebendo inicialmente o valor "0" e o "current" recebe o valor do primeiro objeto, no caso "Lucas"
+//O "return" irá alterar o valor do "prev" para a próxima iteração, fazendo assim a soma das notas.
+const totalGrade = students.reduce( 
+	(prev, current) => {
+		
+		return prev + current.grade;
+	}, 
+	0
+);
+
+//31
+console.log(totalGrade);
+
