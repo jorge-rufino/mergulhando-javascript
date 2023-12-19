22bde0f9243b714e788b76358cf56f2daa719e2d
jorge-rufino <jorge.rufino1986@gmail.com>
2023-12-19 16:04:00 -0300

    Encontrando um registro específico em uma lista com Array.find

--- script.js
+++ script.js
@@ -4,25 +4,20 @@ const students = [
 	{ name: 'Jean',     grade: 10 },
 	{ name: 'Rogerio',  grade: 6 },
 	{ name: 'Marcos',   grade: 5 },
+	{ name: 'Jean',   grade: 9 },
 ];
 
-//Queremos saber os pontos totais da turma, abaixo de 30 pontos precisará de atenção
+//Diferença para o "filter", é que o "find" retorna somente um objeto enquanto o "filter" retorna um array de objetos.
+//"Find" funciona como um "findFirst" pois em caso de valores repetidos, assim que ele encontra o primeiro, ele para de iterar.
+const findStudent = students.find( student => {
 
-//Forma antiga de fazer. Gera efeito colateral pois se executarmos o laço varias vezes vai aumentar o "totalGrade"
-//let totalGrade = 0;
-//students.forEach( student => {	totalGrade += student.grade; });
+	/*		***Pode ser desta forma tb***
+	if(student.name === 'Jean'){
+		return true;
+	}	*/
 
-//Ele recebe 2 parametros, o primeiro é uma função e o segundo é o valor inicial que no caso é 0
-//"Prev" está recebendo inicialmente o valor "0" e o "current" recebe o valor do primeiro objeto, no caso "Lucas"
-//O "return" irá alterar o valor do "prev" para a próxima iteração, fazendo assim a soma das notas.
-const totalGrade = students.reduce( 
-	(prev, current) => {
-		
-		return prev + current.grade;
-	}, 
-	0
-);
+	return student.name === 'Jean';
+});
 
-//31
-console.log(totalGrade);
-
+//Retorna o objeto
+console.log(findStudent);
