a8154d27aa5e930d4c78139c3f612e4068c05c8d
jorge-rufino <jorge.rufino1986@gmail.com>
2023-12-22 09:56:06 -0300

    Async & Await

--- script.js
+++ script.js
@@ -1,24 +1,10 @@
-
-//"resolve" é um método que deve ser usado quando a promise for resolvida
-//"reject" é um método que deve ser usando quando a promisse for rejeitada
-function handleClick () {
-	return new Promise ((resolve, reject) => {
-		setTimeout(() => {
-			//resolve(123);
-			reject('Erro inesperado');
-		}, 5000);
-	});
+function sleep (ms) {
+	return new Promise(
+		resolve => { setTimeout(() => resolve(true), ms); }
+	);
 }
 
-//"then" é um método que só será executado caso a promisse seja resolvida
-//"catch" é um método que só será executado caso a promisse seja rejeitada
-const result = handleClick()
-	.then(res => {
-		console.log(123 === res);
-		console.log('finalizou');
-		return 'batata';
-	})
-	.catch(err => {
-		console.log('houve um erro');
-		console.log(err);
-	});
+async function handleClick () {
+	await sleep(5000);
+	return 'batata';
+}
