8547ebaabcf52fdbbdbc40194cc0e044ac6c1c48
jorge-rufino <jorge.rufino1986@gmail.com>
2023-12-22 10:05:44 -0300

    Promise.all

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
-function sleep (ms) {
-	return new Promise(
-		resolve => { setTimeout(() => resolve(true), ms); }
-	);
+const baseUrl = 'https://jsonplaceholder.typicode.com';
+
+//Executa todos em paralelo e na sequencia, se usassemos o "await", cada promise ficaria esperando finalizar uma para entrar em execuçao
+async function getTodos () {
+	const promises = Promise.all([
+		fetch(`${baseUrl}/todos/1`).then(res => res.json()),
+		fetch(`${baseUrl}/todos/2`).then(res => res.json()),
+		fetch(`${baseUrl}/todos/3`).then(res => res.json()),
+		fetch(`${baseUrl}/todos/4`).then(res => res.json()),
+		fetch(`${baseUrl}/todos/5`).then(res => res.json()),
+		fetch(`${baseUrl}/todos/6`).then(res => res.json()),
+		fetch(`${baseUrl}/todos/7`).then(res => res.json()),
+		fetch(`${baseUrl}/todos/8`).then(res => res.json()),
+		fetch(`${baseUrl}/todos/9`).then(res => res.json()),
+	]);
+
+	return promises;
 }
 
-async function handleClick () {
-	await sleep(5000);
-	return 'batata';
-}
+
+const todos = getTodos();
