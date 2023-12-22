c25dce25962757652660e28fd29f80be09046f16
jorge-rufino <jorge.rufino1986@gmail.com>
2023-12-22 11:39:28 -0300

    Instalando pacote Chalk

--- script.js
+++ script.js
@@ -1,22 +1,13 @@
-fetch('https://jsonplaceholder.typicode.com/todos/1')
-	.then(res => {
-		const data = res.json();			//Transforma a resposta da API em Json
-		console.log('Os dados não aparecem, por que?' + data);	//Porque ".json()" tb é uma "promise"
-	});
+//EXECUTE O ARQUIVO DIRETO NO TERMINAL, PELO HTML ESTÁ DANDO ERRO
 
-fetch('https://jsonplaceholder.typicode.com/todos/1')
-	.then( async (res) => {							//Transformando a promise em "async" 
-		const data = await res.json();		//Esperando executar a promise de transformar em json		
-		console.log(data);								//Agora a resposta json é exibida
-	});	
+import chalk from 'chalk';
 
-//Promise chaining. Usamos 2 "then()", o primeiro executa a promise de transformar em json
-//O segundo captura a resposta json, podendo trabalhar os dados
-fetch('https://jsonplaceholder.typicode.com/todos/1')
-	.then(res => res.json())
-	.then(data => {
-		console.log('userId:'+data.userId);
-		console.log('id:'+data.id);
-		console.log('title:'+data.title);
-		console.log('completed:'+data.completed);
-	});
+console.log(chalk.red('Hello world!'));
+
+const log = console.log;
+
+// Combine styled and normal strings
+log(chalk.blue('Hello') + ' World' + chalk.red('!'));
+
+// Compose multiple styles using the chainable API
+log(chalk.blue.bgRed.bold('Hello world!'));
